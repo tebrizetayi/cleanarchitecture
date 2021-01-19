6e0bb3119f7194a2af91e37da00e76e4a0a9c6ca
tebriz <tebrizetayi@gmail.com>
2021-01-19 01:07:33 +0100

    Mysql repo is added.

--- repository/mysql/author.go
+++ repository/mysql/author.go
@@ -1,1 +1,131 @@
 package mysql
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+	"github.com/tebrizetayi/cleanarchitecture/domain/model"
+)
+
+type AuthorMysqlRepo struct {
+	DB *sql.DB
+}
+
+//root:pass1@tcp(127.0.0.1:3306)/tuts
+func NewAuthorMysqlRepo(conn string) (AuthorMysqlRepo, error) {
+	db, err := sql.Open("mysql", conn)
+	if err != nil {
+		return AuthorMysqlRepo{}, err
+	}
+	return AuthorMysqlRepo{DB: db}, nil
+}
+
+func (a *AuthorMysqlRepo) Create(Authors []model.Author) ([]model.Author, error) {
+
+	result := []model.Author{}
+	query := "Insert into `Author` (`Id`,`Name`) Values "
+	for i, v := range Authors {
+		v.ID = uuid.New()
+
+		if i > 0 {
+			query += ","
+		}
+		query += fmt.Sprintf("('%s','%s')\n", v.ID, v.Name)
+		result = append(result, v)
+	}
+
+	dbrow, err := a.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer dbrow.Close()
+	return result, nil
+}
+
+func (a *AuthorMysqlRepo) GetAll() ([]model.Author, error) {
+
+	query := "Select Id,Name From Author where true=true Limit 10"
+	dbrow, err := a.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer dbrow.Close()
+
+	result := []model.Author{}
+	for dbrow.Next() {
+		var newAuthor model.Author
+		err = dbrow.Scan(&newAuthor.ID, &newAuthor.Name)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, newAuthor)
+	}
+	return result, nil
+}
+
+func (a *AuthorMysqlRepo) Delete(ids []uuid.UUID) error {
+	query := "Delete from  Author where true=true"
+	for _, v := range ids {
+		query += fmt.Sprintf(" AND id=`%s`", v)
+	}
+
+	_, err := a.DB.Exec(query)
+
+	return err
+}
+
+func (a *AuthorMysqlRepo) GetByIds(ids []uuid.UUID) ([]model.Author, error) {
+
+	query := "Select Id, Name from  Author where true=true"
+	for _, v := range ids {
+		query += fmt.Sprintf(" AND Id='%s'", v)
+	}
+
+	dbrow, err := a.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer dbrow.Close()
+
+	result := []model.Author{}
+	for dbrow.Next() {
+		var newAuthor model.Author
+		err = dbrow.Scan(&newAuthor.ID, &newAuthor.Name)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, newAuthor)
+	}
+	return result, nil
+}
+
+func (a *AuthorMysqlRepo) Reset() {
+
+}
+
+func (a *AuthorMysqlRepo) Update(authors []model.Author) ([]model.Author, error) {
+	if authors == nil || len(authors) == 0 {
+		return []model.Author{}, nil
+	}
+
+	updatePhrase := "UPDATE Author SET Name=`%s` WHERE Id=`%s`;"
+	query := ""
+	result := []model.Author{}
+	for _, author := range authors {
+		query += fmt.Sprintf(updatePhrase, author.ID, author.Name)
+	}
+	tx, err := a.DB.BeginTx(context.Background(), nil)
+	if err != nil {
+		return nil, err
+	}
+	tx.Exec(query)
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	return result, nil
+}
--- repository/mysql/author_test.go
+++ repository/mysql/author_test.go
@@ -1,1 +1,74 @@
 package mysql
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/tebrizetayi/cleanarchitecture/domain/model"
+)
+
+func TestAuthorRepository(t *testing.T) {
+	Convey("Setup", t, func() {
+		conn := "root:secret@tcp(127.0.0.1:3306)/Academia"
+		authorrepo, err := NewAuthorMysqlRepo(conn)
+		So(err, ShouldBeNil)
+		Authors := []model.Author{
+			{
+				Name: "John Doe",
+			},
+			{
+				Name: "Jack London",
+			},
+		}
+		created, err := authorrepo.Create(Authors)
+		So(err, ShouldBeNil)
+
+		ids := []uuid.UUID{}
+		for _, v := range created {
+			So(v.ID, ShouldNotBeZeroValue)
+			ids = append(ids, v.ID)
+		}
+
+		//First author is taken for testing
+		author := created[0]
+		Convey("When you update author where the id in the database", func() {
+			author.Name = author.Name + " " + author.Name
+			_, err := authorrepo.Update([]model.Author{author})
+			So(err, ShouldBeNil)
+			Convey("Then updated author can be get by id", func() {
+				updateAuthors, err := authorrepo.GetByIds([]uuid.UUID{author.ID})
+				So(err, ShouldBeNil)
+				So(len(updateAuthors), ShouldEqual, 1)
+				So(updateAuthors[0], ShouldResemble, author)
+			})
+
+		})
+
+		Convey("When you delete author where the id is in the database", func() {
+			err := authorrepo.Delete([]uuid.UUID{author.ID})
+			So(err, ShouldBeNil)
+			Convey("Then the deleted id should not be in the database", func() {
+				deletedAuthors, err := authorrepo.GetByIds([]uuid.UUID{author.ID})
+				So(err, ShouldBeNil)
+				So(len(deletedAuthors), ShouldEqual, 0)
+			})
+		})
+
+		Convey("When you add author", func() {
+			newAuthor := model.Author{
+				Name: "New Author",
+			}
+			newAuthors, err := authorrepo.Create([]model.Author{newAuthor})
+			So(err, ShouldBeNil)
+			So(len(newAuthors), ShouldEqual, 1)
+			So(newAuthors[0].ID, ShouldNotBeNil)
+			Convey("Then the added id should be in the database", func() {
+				authors, err := authorrepo.GetByIds([]uuid.UUID{newAuthors[0].ID})
+				So(err, ShouldBeNil)
+				So(len(authors), ShouldEqual, 1)
+				So(newAuthors, ShouldResemble, authors)
+			})
+		})
+	})
+}
