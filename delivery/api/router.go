7cd18f59360a79e34d41ac64592935846db4a2bb
tebriz <tebrizetayi@gmail.com>
2021-01-26 23:34:53 +0100

    Versionning is added to restapi

--- delivery/api/router.go
+++ delivery/api/router.go
@@ -8,10 +8,10 @@ import (
 
 func InitRoutes(ah *AuthorHandler) *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/author/{id}", ah.Get).Methods(http.MethodGet)
-	r.HandleFunc("/api/author/", ah.Get).Methods(http.MethodGet)
-	r.HandleFunc("/api/author/", ah.Create).Methods(http.MethodPost)
-	r.HandleFunc("/api/author/{id}", ah.Delete).Methods(http.MethodDelete)
-	r.HandleFunc("/api/author/{id}", ah.Update).Methods(http.MethodPut)
+	r.HandleFunc("/api/v1/author/{id}", ah.Get).Methods(http.MethodGet)
+	r.HandleFunc("/api/v1/author/", ah.Get).Methods(http.MethodGet)
+	r.HandleFunc("/api/v1/author/", ah.Create).Methods(http.MethodPost)
+	r.HandleFunc("/api/v1/author/{id}", ah.Delete).Methods(http.MethodDelete)
+	r.HandleFunc("/api/v1/author/{id}", ah.Update).Methods(http.MethodPut)
 	return r
 }
